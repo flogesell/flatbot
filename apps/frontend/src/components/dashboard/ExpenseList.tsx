bd49c276879df91e4ca2bc94c7a62b03a7bc3908
Flo <fgesell@hotmail.de>
2022-10-07 17:50:13 +0200

    virtual list test

--- apps/frontend/src/components/dashboard/ExpenseList.tsx
+++ apps/frontend/src/components/dashboard/ExpenseList.tsx
@@ -1,4 +1,5 @@
-import { Box, Center, List, ListItem, Skeleton, Spinner } from '@chakra-ui/react';
+import { Box, Center, ListItem, Skeleton, Spinner } from '@chakra-ui/react';
+import { List } from 'react-virtualized';
 import React, { useEffect, useRef, useState } from 'react';
 import { Expense as ExpenseType } from '../../models/expenses.model';
 import { useGetExpensesQuery } from '../../services/expense.service';
@@ -8,35 +9,18 @@ export const ExpenseList = () => {
   const { data: expenses, isLoading } = useGetExpensesQuery();
   const [elementsToShow, setElementsToShow] = React.useState(20);
   const [loading, setLoading] = React.useState(false);
-  const [listItems, setListItems] = React.useState<ExpenseType[]>([]);
-  const loader = useRef(null);
 
-  const loadMore = () => {
-    setLoading(true);
-    if (elementsToShow + 20 <= expenses.length) setElementsToShow(elementsToShow + 20);
-    else setElementsToShow(expenses.length);
-  };
-
-  useEffect(() => {
-    setListItems(expenses.slice(0, elementsToShow));
-    setLoading(false);
-  }, [isLoading, elementsToShow, expenses, loading]);
+  useEffect(() => {}, [elementsToShow, expenses, loading]);
 
   if (isLoading) return <Skeleton width="100%"></Skeleton>;
-  return (
-    <List>
-      {!isLoading &&
-        listItems.map((expense) => (
-          <ListItem key={expense.expenseId}>
-            <Expense expense={expense} isLoading={isLoading} />
-          </ListItem>
-        ))}
-      {loading && (
-        <Center>
-          <Spinner />
-        </Center>
-      )}
-      <Box ref={loader} />
-    </List>
+
+  const renderRow = ({ index, key, style }) => (
+    <div>
+      <ListItem>
+        <Expense expense={expenses[index]} isLoading={isLoading} />
+      </ListItem>
+    </div>
   );
+
+  return <List rowCount={expenses.length} rowHeight={45} width={400} height={400} rowRenderer={renderRow} />;
 };
